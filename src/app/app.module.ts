395855ffae52b2885de3838e80dce84c8f766209
Aron Faulhaber <aron.faulhaber@gmail.com>
2021-10-11 12:10:46 +0200

    hero image

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AboutComponent } from './about/about.component';
+import { ContentComponent } from './content/content.component';
+import { HeroComponent } from './hero/hero.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    AboutComponent,
+    ContentComponent,
+    HeroComponent,
   ],
   imports: [
     BrowserModule,
